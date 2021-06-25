ebab44e79f941b9379fdfbd8834ed47083f96b5a
Chisato Nakashima <chisatonakashima@Chisatos-Mac-Book-Pro.local>
2021-06-26 01:43:49 +0900

    add calenderr

--- js/index.js
+++ js/index.js
@@ -75,19 +75,105 @@ function setClickedFormStyle() {
   })
 }
 
-console.log('formLists',formLists)
+//open / close popp window
+
+// document.addEventListener('click',(e)=>{
+//   if (e.target.closest('#locationForm')) {
+//     locationWindow.setAttribute('data-open', 'true');
+//   }else{
+//     locationWindow.setAttribute('data-open', 'false');
+//   }
+// })
+
+function toggleFormWindow(parentNodeSelector, targetElem) {
+  document.addEventListener('click', (e) => {
+    if (e.target.closest(parentNodeSelector)) {
+      targetElem.setAttribute('data-open', 'true');
+    } else {
+      targetElem.setAttribute('data-open', 'false');
+    }
+  })
+}
 
 const locationWindow = document.getElementById('LocationWindow');
+const checkinWindow = document.getElementById('CheckinWindow');
+
+setClickedFormStyle();
+toggleFormWindow('#locationForm', locationWindow);
+toggleFormWindow('#checkinForm', checkinWindow);
 
 
-document.addEventListener('click',(e)=>{
-  if (e.target.closest('#locationForm')) {
-    locationWindow.setAttribute('data-open', 'true');
-  }else{
-    locationWindow.setAttribute('data-open', 'false');
+//set checkin calendar function
+const week = ["日", "月", "火", "水", "木", "金", "土"];
+const today = new Date();
+
+let showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+
+// init
+window.onload = function () {
+  showCalendar(today, 'calendar', 'calendarHeader',calendar);
+  showCalendar(today, 'nextMonthCalendar', 'nextCalendarHeader', calendar);
+};
+
+
+
+// show prev month button
+function showPrevMonth() {
+  showDate.setMonth(showDate.getMonth() - 1);
+  showCalendar(showDate);
+}
+
+// show next month button
+function showNextMonth() {
+  showDate.setMonth(showDate.getMonth() + 1);
+  showCalendar(showDate);
+}
+
+// show calendar
+function showCalendar(date,targetCalenderId,targetHeaderId) {
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  document.getElementById(targetHeaderId).innerHTML = year + "年 " + (month + 1) + "月";
+
+  let calendar = createCalendar(year, month);
+  document.getElementById(targetCalenderId).innerHTML = calendar;
+}
+
+// create calendar
+function createCalendar(year, month) {
+    // 曜日
+    let calendar = "<table><tr class='dayOfWeek'>";
+    for (var i = 0; i < week.length; i++) {
+        calendar += `<th>${week[i]}</th>`;
+    }
+    calendar += "</tr>";
+
+  let count = 0;
+  let startDayOfWeek = new Date(year, month, 1).getDay();
+  let endDate = new Date(year, month + 1, 0).getDate();
+  let lastMonthEndDate = new Date(year, month, 0).getDate();
+  let rowLength = Math.ceil((startDayOfWeek + endDate) / week.length);
+
+  for (let i = 0; i < rowLength; i++) {
+    calendar += "<tr>";
+    for (let j = 0; j < week.length; j++) {
+      if (i == 0 && j < startDayOfWeek) {
+        calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
+      } else if (count >= endDate) {
+        count++;
+        calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+      } else {
+        count++;
+        if (year == today.getFullYear()
+          && month == (today.getMonth())
+          && count == today.getDate()) {
+          calendar += "<td class='today'>" + count + "</td>";
+        } else {
+          calendar += "<td>" + count + "</td>";
+        }
+      }
+    }
+    calendar += "</tr>";
   }
-})
-
-setClickedFormStyle();s
-
-
+  return calendar;
+}
